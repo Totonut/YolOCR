1339c0a754119ecbef59cf099b39ad82ab6204ce
imyke <faillo_m@epita.fr>
2014-10-27 02:39:15 +0100

    petit nottoyage

--- interface.c
+++ interface.c
@@ -2,20 +2,18 @@
 #include <gtk/gtk.h>
 #include "interface.h"
 
-void open_window( int argc, char* argv[], SDL_Surface *image)
+void open_window(int argc,char* argv[], SDL_Surface *image)
 {
-    GtkWidget *pWindow,*pWindow2;
-    GtkWidget *pVBox, *pHBox;
-    GtkWidget *pImage, *pImage2;
+    GtkWidget *pWindow, *pVBox, *pHBox, *pImage, *pImage2;
 	GdkPixbuf *pixbuf, *pixbuf2;
-    unsigned ok=0;
-	GtkWidget *pQuitImage,*buttonFindChar;
-    GtkWidget *pQuitBtn,*pEntry;
+	GtkWidget *buttonFindChar;
+    
 	GError *error=NULL;
  
     pWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(pWindow), "GtkImage");
-    g_signal_connect(G_OBJECT(pWindow), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(G_OBJECT(pWindow), "destroy", G_CALLBACK(gtk_main_quit),
+			NULL);
 
 		
  	pHBox= gtk_hbox_new(FALSE,8);
@@ -24,38 +22,21 @@ void open_window( int argc, char* argv[], SDL_Surface *image)
  	gtk_container_add(GTK_CONTAINER(pWindow),pHBox);
 	pixbuf2=gdk_pixbuf_new_from_file("image2.bmp", &error);
 	pixbuf = gdk_pixbuf_new_from_file ("image.bmp", &error); 
-	if (!error) 
-	  { 
-    	GdkPixbuf *pixbuf_mini = NULL; 
-		GdkPixbuf *pixbuf_mini2=NULL;
-		pixbuf_mini2=gdk_pixbuf_scale_simple(pixbuf2,650,450,
-		GDK_INTERP_NEAREST);
-  
-    	pixbuf_mini = gdk_pixbuf_scale_simple (pixbuf,650, 450, 
-         GDK_INTERP_NEAREST); 
-   		 pImage2=gtk_image_new_from_pixbuf(pixbuf_mini2);
-   		 pImage = gtk_image_new_from_pixbuf (pixbuf_mini);
-  }
-		
-	
-	
-	
-	  buttonFindChar = gtk_button_new_with_label("FindChar");
-   	  gtk_box_pack_start(GTK_BOX(pVBox), buttonFindChar, FALSE,TRUE , 0);
-	  g_signal_connect(G_OBJECT(buttonFindChar), "clicked",
+    GdkPixbuf *pixbuf_mini = NULL; 
+	GdkPixbuf *pixbuf_mini2=NULL;
+	pixbuf_mini2=gdk_pixbuf_scale_simple(pixbuf2,650,450,
+	GDK_INTERP_NEAREST);
+    pixbuf_mini = gdk_pixbuf_scale_simple (pixbuf,650, 450, 
+            GDK_INTERP_NEAREST); 
+    pImage2=gtk_image_new_from_pixbuf(pixbuf_mini2);
+    pImage = gtk_image_new_from_pixbuf (pixbuf_mini);
+    buttonFindChar = gtk_button_new_with_label("FindChar");
+    gtk_box_pack_start(GTK_BOX(pVBox), buttonFindChar, FALSE,TRUE , 0);
+    g_signal_connect(G_OBJECT(buttonFindChar), "clicked",
 			  G_CALLBACK(afficher_findchar), NULL);
-
- 
-	
-	
-	
-	
-	
-	gtk_container_set_border_width (GTK_CONTAINER (pWindow), 30);
-
+ 	gtk_container_set_border_width (GTK_CONTAINER (pWindow), 30);
 	gtk_window_set_default_size(GTK_WINDOW(pWindow), 200, 200);
-	 //gtk_widget_set_size_request(pImage, 200, 200);
-	 gtk_window_move(GTK_WINDOW(pWindow),  0,  600);
+    gtk_window_move(GTK_WINDOW(pWindow),  0,  600);
 	gtk_box_pack_start(GTK_BOX(pHBox), pImage, FALSE, TRUE, 0);
 	gtk_box_pack_start(GTK_BOX(pHBox),pImage2,FALSE,TRUE,0);
 	gtk_box_pack_start(GTK_BOX(pVBox),pHBox,FALSE,TRUE,0);
@@ -66,19 +47,17 @@ void open_window( int argc, char* argv[], SDL_Surface *image)
 
 void afficher_findchar()
 {
-	GtkWidget *pWindow, *pEntry, *pVBox,*view;
+	GtkWidget *pWindow, *pVBox,*view;
 	pWindow= gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_move(GTK_WINDOW(pWindow),  50,  80);
 	g_signal_connect(G_OBJECT(pWindow), "delete-event",
 			G_CALLBACK(gtk_widget_hide_on_delete),G_OBJECT(pWindow));
-   view = gtk_text_view_new(); 
-	
+    view = gtk_text_view_new(); 
 	pVBox = gtk_vbox_new(TRUE, 0);
     gtk_container_add(GTK_CONTAINER(pWindow), pVBox);
 	gtk_box_pack_start(GTK_BOX(pVBox), view, TRUE, TRUE, 0);
 	gtk_window_set_default_size(GTK_WINDOW(pWindow), 700, 450);
-
-	 gtk_widget_show_all(pWindow);
+    gtk_widget_show_all(pWindow);
  
 
 
--- main.c
+++ main.c
@@ -9,20 +9,16 @@ int main(int argc, char *argv[])
 	
     gtk_init(&argc, &argv);
     SDL_Surface *image = NULL;
-    unsigned* chars = NULL;
 
     if (argc==2)
 		image = IMG_Load(argv[1]);
     else
 		image = IMG_Load("image.jpg");
 	SDL_SaveBMP(image, "image.bmp");
-	printf("BP1 !!!%s\n", image);
 	perfectImage(image);
-	printf("BP2 !!!\n");
 	findChar(image);
-	printf("BP3 !!!\n");
 	SDL_SaveBMP(image, "image2.bmp");
-	open_window(&argc, &argv, image);
+	open_window(argc, argv, image);
 	
 	SDL_FreeSurface(image);	
 	
