c65342d353c3dcbaac02ef1f5601eaa0ede889cc
Alexandre Chabrolin <chabro_a@epita.fr>
2014-12-08 11:27:09 +0100

    ?

--- interface.h
+++ interface.h
@@ -5,7 +5,6 @@
 //#include <stdlib.h>
 #include <gtk/gtk.h>
 #include "findChar.h"
-
 void afficher_findchar();
 
 struct newImage
@@ -14,17 +13,6 @@ struct newImage
 	GdkPixbuf* pixbuf;
 };
 void on_copier_button(gpointer data);
-void create_window();
-static void menu_new (void);
-static gchar * menu_open (void);
-static void menu_quit (void);
-static void menu_help (void);
-static void menu_about (void);
-//static void menu_addWidget (GtkUIManager *,GtkWidget *, GtkContainer *);
-static void menu_addWidget (GtkWidget *, GtkContainer *);
-void main2();
-void factorize(GtkWidget *p_vBox);
-void open_image(gchar *sChemin ,GtkWidget *p_vBox);
-static void Binarize (void);
+void open_window();
 
 #endif
--- main.c
+++ main.c
@@ -1,11 +1,6 @@
 #include "constantes.h"
 #include "perfectImage.h"
 #include "interface.h"
-#include "findChar.h"
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <SDL/SDL.h>
 
 
 int main(int argc, char *argv[])
@@ -29,10 +24,9 @@ int main(int argc, char *argv[])
   else
 	image = IMG_Load("image.jpg");
 	SDL_SaveBMP(image, "image.bmp");
-
 	
   perfectImage(image);
- 	char *string = find_char(image, net, layers_Size, layers, chars);
+ 	char *string =  find_char(image, net, layers_Size, layers, chars);
 	SDL_SaveBMP(image, "image2.bmp");
 
   printf("%s\n", string);
@@ -40,13 +34,6 @@ int main(int argc, char *argv[])
   open_window();
 
   SDL_FreeSurface(image);	
-
-	perfectImage(image);
- 	find_char(image);
-	SDL_SaveBMP(image, "image2.bmp");
-    	create_window();
-		SDL_FreeSurface(image);	
-
 	
 	SDL_Quit();
 	return 0;
